2c327b51927a3995858c0a385d7431c8d4d56d92
yaam <yaam17@outlook.com>
2023-04-17 13:51:25 -0500

    :poop:

--- db/db-connection-mongo.js
+++ db/db-connection-mongo.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const getConnection = async () =>{
     try {
         console.log('Inicializando llamado a bd');
-    await mongoose.connect(process.env.MONGO_URI ,  {
+    await mongoose.connect(process.env.MONGO_URI) ,  {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
