d007d36aa3eb2a667d148fb434a2730a2c516bb5
yaam <yaam17@outlook.com>
2023-04-17 11:53:11 -0500

    modificación en la conexión de mongo db :sparkles:

--- db/db-connection-mongo.js
+++ db/db-connection-mongo.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const getConnection = async () =>{
     try {
         console.log('Inicializando llamado a bd');
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect('mongodb://skyway:qoX6eyytQKmgYs0v@cluster0-shard-00-00.kmx9g.mongodb.net:27017,cluster0-shard-00-01.kmx9g.mongodb.net:27017,cluster0-shard-00-02.kmx9g.mongodb.net:27017/inventarios?ssl=true&replicaSet=atlas-i3zcwb-shard-0&authSource=admin&retryWrites=true&w=majority', {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
